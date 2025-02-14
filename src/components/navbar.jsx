98da83a0585bb1bb0e07cb272b9d3cdf0e45d1ae
Dhruba Chakraborty <dhruba.chakraborty.2000@gmail.com>
2025-02-14 15:17:14 +0600

    ui: styling navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,22 @@
+import searchIcon from "../../public/assets/search.svg";
+import fbIcon from "../../public/assets/fb.svg";
+import igIcon from "../../public/assets/instagram.svg";
+import twitterIcon from "../../public/assets/twitter.svg";
+import pinterestIcon from "../../public/assets/pinterest.svg";
 const Navbar = () => {
   return (
-    <div className="bg-black flex flex-row justify-between text-white">
-      <div id="col-1" className="flex flex-row px-4 gap-1">
-        <p>icon</p>
+    <div className="bg-[#F7F3F0] flex flex-row justify-between items-center text-black px-5">
+      <div
+        id="col-1"
+        className="flex flex-row px-4 gap-2 py-2 justify-center items-center"
+      >
+        <img src="assets/subscribe-icon.png" alt="" srcset="" />
         <p>NEWSLETTER</p>
       </div>
-      <div id="col-2" className="flex flex-row px-4 gap-1">
+      <div
+        id="col-2"
+        className="flex flex-row px-4 gap-1 w-[25%] items-center justify-between"
+      >
         <p>HOME</p>
         <p>PAGES</p>
         <p>SHOP</p>
@@ -13,14 +24,15 @@ const Navbar = () => {
       </div>
       <div id="col-3" className="flex flex-row px-4 gap-4">
         <div className="flex flex-row gap-1">
-          <p>icon</p>
+          <img src={searchIcon} className="w-5" />
           <p>SEARCH</p>
         </div>
-        <div className="flex flex-row gap-1">
-          <p>F</p>
-          <p>I</p>
-          <p>T</p>
-          <p>P</p>
+        <div className="flex flex-row gap-1"></div>
+        <div className="flex flex-row gap-4">
+          <img src={fbIcon} className="w-5" />
+          <img src={igIcon} className="w-5" />
+          <img src={twitterIcon} className="w-5" />
+          <img src={pinterestIcon} className="w-5" />
         </div>
       </div>
     </div>
