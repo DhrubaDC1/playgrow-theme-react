98fce8cb04e263fdde2025723254f1b2ec93922a
Dhruba Chakraborty <dhruba.chakraborty.2000@gmail.com>
2025-02-16 01:15:05 +0600

    cursor pointer for navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@ import fbIcon from "/assets/fb.svg";
 import igIcon from "/assets/instagram.svg";
 import twitterIcon from "/assets/twitter.svg";
 import pinterestIcon from "/assets/pinterest.svg";
+
 const Navbar = () => {
   return (
     <div className="bg-[#F7F3F0] flex flex-row justify-between items-center text-black px-5 w-full">
@@ -11,16 +12,16 @@ const Navbar = () => {
         className="flex flex-row px-4 gap-2 py-3 justify-center items-center"
       >
         <img src="assets/subscribe-icon.png" alt="" srcset="" />
-        <p>NEWSLETTER</p>
+        <button className="cursor-pointer">NEWSLETTER</button>
       </div>
       <div
         id="col-2"
         className="flex flex-row px-4 gap-1 w-[25%] items-center justify-between"
       >
-        <p>HOME</p>
-        <p>PAGES</p>
-        <p>SHOP</p>
-        <p>BLOG</p>
+        <button className="cursor-pointer">HOME</button>
+        <button className="cursor-pointer">PAGES</button>
+        <button className="cursor-pointer">SHOP</button>
+        <button className="cursor-pointer">BLOG</button>
       </div>
       <div id="col-3" className="flex flex-row px-4 gap-4">
         <div className="flex flex-row gap-1">
@@ -28,7 +29,7 @@ const Navbar = () => {
           <p>SEARCH</p>
         </div>
         <div className="flex flex-row gap-1"></div>
-        <div className="flex flex-row gap-4">
+        <div className="flex flex-row gap-4 cursor-pointer">
           <img src={fbIcon} className="w-5" />
           <img src={igIcon} className="w-5" />
           <img src={twitterIcon} className="w-5" />
